86bc44d28e956f4973e9622624f2da8323a6530c
azfar.imtiaz <azfar.imtiaz@live.com>
2023-07-26 13:13:10 +0200

    Make new item view disappear upon clicking Save button

--- ToDoList/Views/NewItemView.swift
+++ ToDoList/Views/NewItemView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct NewItemView: View {
+    @StateObject var viewModel = NewItemViewViewModel()
+    @Binding var newItemPresented: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("New Item")
+                .font(.system(size: 32))
+                .bold()
+                .padding()
+            
+            Form {
+                // Title
+                TextField("Title", text: $viewModel.title)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                // Due Date
+                DatePicker("Due Date", selection: $viewModel.dueDate)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                
+                // Save Button
+                TLButton(title: "Save", backgroundColor: .pink) {
+                    viewModel.save()
+                    newItemPresented = false
+                }
+                .padding()
+            }
+        }
     }
 }
 
 struct NewItemView_Previews: PreviewProvider {
     static var previews: some View {
-        NewItemView()
+        NewItemView(newItemPresented: Binding(get: {
+            return true
+        }, set: {
+            _ in
+        }))
     }
 }
