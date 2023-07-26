8eb860910c7f6a3192bc352916aa72b3e78186a8
azfar.imtiaz <azfar.imtiaz@live.com>
2023-07-26 18:25:08 +0200

    Fix login bug, add app icon and launch screen

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -12,10 +12,63 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Button("Log Out") {
+                        viewModel.logOut()
+                    }
+                    Text("Loading user...")
+                }
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        // Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        // Info: Name, Email, Member since
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            HStack {
+                Text("Email Address: ")
+                    .bold()
+                Text(user.emailAddress)
+            }
+            .padding()
+            HStack {
+                Text("Member since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joinDate).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        .padding()
+        
+        // Sign out button
+        Button("Log Out") {
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+        
+        Spacer()
     }
 }
 
