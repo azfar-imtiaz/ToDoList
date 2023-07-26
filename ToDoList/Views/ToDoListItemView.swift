38ee03a8c49f293bbaf84bd9f2873ff2680fa242
azfar.imtiaz <azfar.imtiaz@live.com>
2023-07-26 16:47:20 +0200

    Display user's items when he/she logs in

--- ToDoList/Views/ToDoListItemView.swift
+++ ToDoList/Views/ToDoListItemView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct ToDoListItemView: View {
+    @StateObject var viewModel = ToDoListItemViewViewModel()
+    let item: ToDoListItem
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text(item.title)
+                    .font(.body)
+                    .padding(.bottom, 5)
+                
+                Text("\(Date(timeIntervalSince1970: item.dueDate).formatted(date: .abbreviated, time: .shortened))")
+                    .font(.footnote)
+                    .foregroundColor(item.dueDate.isLess(than: Date().timeIntervalSince1970) ? Color.red : Color(.secondaryLabel))
+            }
+            
+            Spacer()
+            
+            Button {
+                viewModel.toggleIsDone(item: item)
+            } label: {
+                Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
+            }
+        }
     }
 }
 
 struct ToDoListItemView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListItemView()
+        ToDoListItemView(item: ToDoListItem(id: "17HZ03u2R3UFV75wVPjK5oh0PeF3", title: "Buy bread", dueDate: Date().timeIntervalSince1970, createdDate: Date().timeIntervalSince1970, isDone: false))
     }
 }
--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -6,19 +6,32 @@
 //
 
 import SwiftUI
+import FirebaseFirestoreSwift
 
 struct ToDoListView: View {
     @StateObject var viewModel = ToDoListViewViewModel()
+    @FirestoreQuery var items: [ToDoListItem]
     
     private let userId: String
     init(userId: String) {
         self.userId = userId
+        self._items = FirestoreQuery(
+            collectionPath: "users/\(userId)/ToDos")
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) { item in
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button("Delete") {
+                                viewModel.delete(itemID: item.id)
+                            }
+                            .tint(Color.red)
+                        }
+                }
+                .listStyle(.plain)
             }
             .navigationTitle("To Do List")
             .toolbar {
@@ -38,6 +51,6 @@ struct ToDoListView: View {
 
 struct ToDoListView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListView(userId: "")
+        ToDoListView(userId: "17HZ03u2R3UFV75wVPjK5oh0PeF3")
     }
 }
